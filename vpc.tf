4070019772bc79af335840845b1506d97dc89e33
oayoade <83650324+oayoade@users.noreply.github.com>
2024-12-07 22:03:44 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "olu-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -16,9 +16,9 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  version = "5.7.1"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "olu-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
